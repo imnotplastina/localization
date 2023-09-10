b7e1dbe6f3a194548622f116f16956a5228999b2
Islam Bilalov <bilalov1504@gmail.com>
2023-09-10 13:11:09 +0300

    feat: add route for language controller

--- routes/main.php
+++ routes/main.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'admin.home.index')->name('home');
+Route::view('/', 'home.index')->name('home');
+
+Route::get('/language/{language}', LanguageController::class)->name('language');
