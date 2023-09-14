afda320f053891fe84d14f68fee652b483051f40
Islam Bilalov <bilalov1504@gmail.com>
2023-09-14 20:55:56 +0300

    feat: give language prefix for route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Language;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -19,12 +20,16 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('web')
-                ->group(base_path('routes/main.php'));
+            Route::prefix(Language::routePrefix())
+                ->group(function () {
+                    Route::middleware('web')
+                        ->group(base_path('routes/main.php'));
 
-            Route::middleware('web')
-                ->prefix('admin')->name('admin.')
-                ->group(base_path('routes/admin.php'));
+                    Route::middleware('web')
+                        ->prefix('admin')->name('admin.')
+                        ->group(base_path('routes/admin.php'));
+                });
+
 
             Route::middleware('api')
                 ->prefix('api')
