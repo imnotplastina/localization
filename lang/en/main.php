017c0ef5fe79548a45602bed8c3313fe4dca4071
Islam Bilalov <bilalov1504@gmail.com>
2023-09-27 19:22:39 +0300

    feat: lang text containers

---
+++ lang/en/main.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => 'Main',
+    'content' => 'Main page content',
+];
---
+++ lang/ru/main.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => 'Главная',
+    'content' => 'Контен главной страницы',
+];
