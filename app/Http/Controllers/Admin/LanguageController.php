60592bd2ba58e8337ac52262fcd73443418a476d
Islam Bilalov <bilalov1504@gmail.com>
2023-09-27 18:45:43 +0300

    refactor: add return types

--- app/Http/Controllers/Admin/LanguageController.php
+++ app/Http/Controllers/Admin/LanguageController.php
@@ -5,24 +5,26 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\SaveLanguageRequest;
 use App\Models\Language;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class LanguageController extends Controller
 {
-    public function index()
+    public function index(): View
     {
         return view('admin.languages.index', [
             'languages' => Language::all(),
         ]);
     }
 
-    public function create()
+    public function create(): View
     {
         return view('admin.languages.create', [
             'language' => new Language,
         ]);
     }
 
-    public function store(SaveLanguageRequest $request)
+    public function store(SaveLanguageRequest $request): RedirectResponse
     {
         $validated = $request->validated();
 
@@ -31,14 +33,14 @@ class LanguageController extends Controller
         return to_route('admin.languages.index');
     }
 
-    public function edit(Language $language)
+    public function edit(Language $language): View
     {
         return view('admin.languages.edit', [
             'language' => $language,
         ]);
     }
 
-    public function update(SaveLanguageRequest $request, Language $language)
+    public function update(SaveLanguageRequest $request, Language $language): RedirectResponse
     {
         $validated = $request->validated();
 
@@ -51,7 +53,7 @@ class LanguageController extends Controller
         return to_route('admin.languages.index');
     }
 
-    public function destroy(Language $language)
+    public function destroy(Language $language): RedirectResponse
     {
         $language->delete();
 
