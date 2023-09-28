18109e38d3ee32cb4ced977e51c0378e2a385559
Islam Bilalov <bilalov1504@gmail.com>
2023-09-28 18:12:02 +0300

    feat: pluralization group

---
+++ lang/en/pluralization.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => 'Pluralization',
+    'content' => 'You have :count product in your cart|You have :count products in your cart',
+];
---
+++ lang/ru/pluralization.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'title' => 'Плюрализация',
+    'content' => 'В корзине :count товар|В корзине :count товара|В корзине :count товаров',
+];
