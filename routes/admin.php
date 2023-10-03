ea4428112106b0f95c255eac13a82f63b899f4fb
Islam Bilalov <bilalov1504@gmail.com>
2023-10-03 15:50:35 +0300

    feat: translation resource route

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Admin\LanguageController;
+use App\Http\Controllers\Admin\TranslationController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'admin.home.index')->name('home');
 
 Route::resource('languages', LanguageController::class);
+Route::resource('translations', TranslationController::class);
